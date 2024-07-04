e6f993a90b9006a22a42c0569b5f4e574fc72444
ahmed abbassi <abbassia082@gmail.com>
2024-07-04 14:49:11 +0100

    added search by name email feature

--- force-app/main/default/aura/communities/communitiesController.js
+++ force-app/main/default/aura/communities/communitiesController.js
@@ -1,18 +1,33 @@
 ({
+    //get all users when the page is first loaded
     // eslint-disable-next-line no-unused-vars
     init : function(component, event, helper) {
         helper.allcommunities(component);
         helper.allUserName(component);
         component.set('v.mycolumns', [
             { label: 'user Name', fieldName: 'Name', type: 'text'},
-            
+            { label: 'Id', fieldName: 'Id', type: 'text'},
+            { label: 'Profile', fieldName: 'ProfileName', type: 'text'},
+            { label: 'Account', fieldName: 'AccountName', type: 'text'},
+           
         ]);
     },
-
+    //select bar change to get users by community name
      handleChange: function(component, event, helper) {
         var selectedNetworkId = event.getSource().get("v.value");
         component.set("v.selectedNetworkId", selectedNetworkId);
-        helper.allUserName(component, selectedNetworkId);
+        helper.allUserName(component, null);
     },
+
+    //search input onChange to get users
+    handleInputChange: function(component, event,helper){
+       
+        var inputData = event.getParam('value');
+        component.set("v.textValue", inputData);
+      
+        helper.allUserName(component,event.getParam('value'));
+        
+        
+    }
 
 })
--- force-app/main/default/aura/communities/communitiesHelper.js
+++ force-app/main/default/aura/communities/communitiesHelper.js
@@ -4,23 +4,23 @@
         var action = component.get("c.getCommunities");
         action.setCallback(this,function(response)
         {
-            // eslint-disable-next-line no-console
-            console.log('state', response.getState());
+            
             component.set("v.netList",response.getReturnValue()) ; 
         });
 
     $A.enqueueAction(action);
     },
 
-    allUserName : function(component, networkId){
+    allUserName : function(component, userName){
+    //    var userName= component.get("v.textValue");
+       var  networkId = component.get("v.selectedNetworkId");
+       
         var action = component.get('c.getCommunityUserByProfiles');
-        action.setParams({ networkId: networkId });
+        action.setParams({ networkId: networkId , userName:userName});
         action.setCallback(this, function(response){
             if(response.getState()==='SUCCESS'){
-                // eslint-disable-next-line no-console
-                console.log('true->',  response.getReturnValue());
-                // eslint-disable-next-line no-console
-                console.log('selected rows->', networkId);
+                
+                
                 component.set('v.mydata',response.getReturnValue());
             }
             else if (response.getState() === "ERROR") {
