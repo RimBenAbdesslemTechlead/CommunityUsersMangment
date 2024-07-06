ddf28e843ff30dd33dcc5acd3b064450c9dbada9
ahmed abbassi <abbassia082@gmail.com>
2024-07-06 15:01:49 +0100

    create user feature

--- force-app/main/default/aura/addUserForm/addUserFormController.js
+++ force-app/main/default/aura/addUserForm/addUserFormController.js
@@ -1,5 +1,22 @@
+/* eslint-disable no-console */
 ({
-    myAction : function(component, event, helper) {
+    init: function() {
+        // eslint-disable-next-line no-console
+        console.log('Component Initialized');
+    },
 
+    createUser : function(component, event, helper){
+        var lastname= component.find('lastname').get("v.value");
+        var email = component.find('email').get("v.value");
+        var alias = component.find('alias').get("v.value");
+        var username = component.find('username').get("v.value")
+        var tz = component.find('tz').get("v.value");
+        var loc = component.find('loc').get("v.value")
+        var emlenco = component.find('emlenco').get("v.value");
+        var lang = component.find('lang').get("v.value")
+        
+        console.log('lastname c', lastname, email, alias, username, tz,loc,emlenco, lang);
+
+        helper.createNewUser(component,lastname, email, alias, username, tz,loc,emlenco, lang);
     }
 })
--- force-app/main/default/aura/addUserForm/addUserFormHelper.js
+++ force-app/main/default/aura/addUserForm/addUserFormHelper.js
@@ -1,5 +1,20 @@
+/* eslint-disable no-console */
 ({
-    helperMethod : function() {
-
+    createNewUser : function(component, lastname, email, alias, username, tz,loc,emlenco, lang) {
+        console.log('latname h', lastname,'email', email, 'alias',alias, 'usernname', username);
+        var action = component.get('c.getNewUser');
+        action.setParams({
+            name :lastname,email : email,alias:alias, username: username, tz:tz, loc:loc,emlenco : emlenco,lang: lang
+        });
+        action.setCallback(this, function(response){
+            if(response.state==='SUCCESS'){
+               // eslint-disable-next-line no-console
+            console.log("response", response.getReturnValue());
+           }else{
+               // eslint-disable-next-line no-console
+               console.log(response.error);
+           }
+        })
+    $A.enqueueAction(action);
     }
 })
